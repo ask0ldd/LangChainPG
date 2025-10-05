7b391395e2889f39e8f1242c793e9288de8e083a
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-10-05 03:38:55 +0200

    *del* front / unused agent page tools

--- src/app/(5-agent)/agent/_schemas/addInputSchema.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod";
-
-const addInputSchema = z.object({
-    a: z.number(),
-    b: z.number(),
-});
-
-export default addInputSchema
--- src/app/(5-agent)/agent/_schemas/multiplyInputSchema.ts
+++
@@ -1,8 +0,0 @@
-import z from "zod"
-
-const multiplyInputSchema = z.object({
-    a: z.number(),
-    b: z.number(),
-})
-
-export default multiplyInputSchema
