c5e2271bee08c65db45fcc357c90930c5862c5d6
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-10-06 13:46:59 +0200

    *add* front / graph page : order retrieval tool & schema

---
+++ src/app/(6-graph)/graph/_tools/orderRetrievalTool.ts
@@ -0,0 +1,25 @@
+import { tool } from "@langchain/core/tools";
+import orderRetrieveInputSchema from "../_schemas/orderRetrievalInputSchema";
+
+const orderRetrievalTool = tool(
+    async ({customerName, orderId}) => {
+        if(orderId) {
+            console.log('orderId :', orderId)
+            return ({id : orderId, product : '3 black shirts', customer : 'ced', date : Date.now})
+        }
+
+        if(customerName?.toLowerCase().includes("ced")) {
+            console.log('customerName :', customerName)
+            return ({id : 123456, product : '2 cargo shorts', customer : 'ced', date : Date.now, status : 'Payment has been rejected due to insufficient funds on the cutsomer account.'})
+        }
+
+        return "no result"
+    },
+    {
+        name: "orderRetrieve",
+        schema: orderRetrieveInputSchema,
+        description: "Retrieve an order using its orderId or the last order for a specific customerName.", 
+    }
+)
+
+export default orderRetrievalTool
