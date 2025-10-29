b4985a75906476f80ea8a1135e33bc6d2fd4cdbc
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-10-29 19:08:27 +0100

    *edit* front / schema : agenda input

--- src/app/(9-booking)/booking/_schemas/agendaInputSchema.ts
+++ src/app/(9-booking)/booking/_schemas/agendaInputSchema.ts
@@ -1,7 +1,11 @@
 import { z } from "zod/v4"
 
+const timeRegex = /^([01]\d|2[0-3]):[0-5]\d:[0-5]\d$/;
+
 const agendaInputSchema = z.object({
-  customerAvailability: z.string(),
+  availableFrom: z.string().regex(timeRegex, "Time must be in HH:MM:SS format")/*.datetime()*/.optional(),
+  availableTo: z.string().regex(timeRegex, "Time must be in HH:MM:SS format")/*.datetime()*/.optional(),
+  availableDay: z.string().optional(),
 })
 
 export default agendaInputSchema
