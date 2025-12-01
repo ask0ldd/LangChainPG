6a089ec26834276502a82db7c82850e824aecca0
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-12-01 14:25:18 +0100

    *add* front / supervisor : send email tool

---
+++ src/app/(9-supervisor)/supervisor/_tools/sendEmailTool.ts
@@ -0,0 +1,14 @@
+import { tool } from "@langchain/core/tools";
+import sendEmailSchema from "../_schemas/sendEmailSchema";
+
+const sendEmail = tool(
+  async ({ to, subject, body, cc }) => {
+    // !!! Stub: In practice, this would call SendGrid, Gmail API, etc.
+    return `Email sent to ${to.join(', ')} - Subject: ${subject}`;
+  },
+  {
+    name: "send_email",
+    description: "Send an email via email API. Requires properly formatted addresses.",
+    schema: sendEmailSchema,
+  }
+)
