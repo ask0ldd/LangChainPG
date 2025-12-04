0e3b0de3b44b054f3b701e730b16b7931fe274c7
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-12-04 19:46:20 +0100

    *edit* front / agenda & item id schemas : descriptions added

--- src/app/(7-advancedgraph)/advgraph/_schemas/itemIdentificationInputSchema.ts
+++ src/app/(7-advancedgraph)/advgraph/_schemas/itemIdentificationInputSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod/v4"
 
 const orderRetrieveInputSchema = z.object({
-  itemDescription: z.string(),
+  itemDescription: z.string().describe("Natural language description of an item"),
 })
 
 export default orderRetrieveInputSchema
