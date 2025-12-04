d9e5fbad5c59467cdb0bb204ddf3c34a93f5a760
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-12-04 19:46:51 +0100

    *edit* front / sentiment input & order retrieval schema : description added

--- src/app/(6-graph)/graph/_schemas/sentimentInputSchema.ts
+++ src/app/(6-graph)/graph/_schemas/sentimentInputSchema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod/v4"
 
 const SentimentInputSchema = z.object({
-    message: z.string(), 
+    message: z.string().describe('Natural language message related to a product'), 
 })
 
 export default SentimentInputSchema
