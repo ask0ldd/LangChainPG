3b3a4da1af6664e1dbfa2213b7a46af52f8483d0
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-12-06 18:45:21 +0100

    *edit* front / add input schema : description added

--- src/app/(2-toolcalling)/toolcalling/_schemas/addInputSchema.ts
+++ src/app/(2-toolcalling)/toolcalling/_schemas/addInputSchema.ts
@@ -1,8 +1,8 @@
 import z from "zod";
 
 const addInputSchema = z.object({
-    a: z.number(),
-    b: z.number(),
+    a: z.number().describe('First number to be added'),
+    b: z.number().describe('Second number to be added'),
 });
 
 export default addInputSchema
