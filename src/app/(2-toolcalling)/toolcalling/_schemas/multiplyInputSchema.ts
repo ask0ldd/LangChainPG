9ff30bec8fb503029e5e2c6f2fd7b33a7b0faa53
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-12-06 18:45:32 +0100

    *edit* front / multiply input schema : description added

--- src/app/(2-toolcalling)/toolcalling/_schemas/multiplyInputSchema.ts
+++ src/app/(2-toolcalling)/toolcalling/_schemas/multiplyInputSchema.ts
@@ -1,8 +1,8 @@
 import z from "zod"
 
 const multiplyInputSchema = z.object({
-    a: z.number(),
-    b: z.number(),
+    a: z.number().describe('First number to be multiplied'),
+    b: z.number().describe('Second number to be multiplied'),
 })
 
 export default multiplyInputSchema
